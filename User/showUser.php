ffc0219df6e5ea355e1496383fa4b56dae04df3a
Glodi Mietete <glodimietete@gmail.com>
2025-03-14 23:11:27 +0100

    Création de la page ShowUser

---
+++ User/showUser.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+
+<body>
+
+    <main>
+        <div class="link_container">
+            <a href="link" href="addUser.php">Ajouter un utilisateur</a>
+
+        </div>
+
+        <table>
+            <thead>
+                <tr>
+                    <th>Nom</th>
+                    <th>Email</th>
+                    <th>Modifier</th>
+                    <th>Supprimer</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td><a class="image" href="modifyUser.php?="> <img src="../images/write.png" alt=""></a></td>
+                    <td><a class="image" href="deleteUser.php?="> <img src="../images/remove.png" alt=""></a></td>
+                </tr>
+            </tbody>
+        </table>
+
+    </main>
+
+</body>
+
+</html>
