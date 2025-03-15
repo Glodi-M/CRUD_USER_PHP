f77d3a22b9c970c73d70cd0d583f18bdcf829cf3
Glodi Mietete <glodimietete@gmail.com>
2025-03-15 11:35:49 +0100

    Opération CRUD (READ) Affichage de données dans la table via la BD et la connexion avec la BD

--- User/showUser.php
+++ User/showUser.php
@@ -12,27 +12,54 @@
 
     <main>
         <div class="link_container">
-            <a href="link" href="addUser.php">Ajouter un utilisateur</a>
+            <a href="addUser.php">Ajouter un utilisateur</a>
 
         </div>
 
         <table>
             <thead>
-                <tr>
-                    <th>Nom</th>
-                    <th>Email</th>
-                    <th>Modifier</th>
-                    <th>Supprimer</th>
-                </tr>
+                <?php
+                include('../connect_ddb.php');
+
+                // Récupérer les données de la table users
+                $sql = "SELECT * FROM users";
+                $result = mysqli_query($conn, $sql);
+
+                if (mysqli_num_rows($result) > 0) {
+                    // Afficher les données de chaque ligne
+                ?>
+                    <tr>
+                        <th>Nom</th>
+                        <th>Email</th>
+                        <th>Modifier</th>
+                        <th>Supprimer</th>
+                    </tr>
             </thead>
 
             <tbody>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td><a class="image" href="modifyUser.php?="> <img src="../images/write.png" alt=""></a></td>
-                    <td><a class="image" href="deleteUser.php?="> <img src="../images/remove.png" alt=""></a></td>
-                </tr>
+                <?php
+                    while ($row = mysqli_fetch_assoc($result)) {
+                ?>
+                    <tr>
+                        <td><?php echo $row['username']; ?></td>
+                        <td><?php echo $row['email']; ?></td>
+                        <td class="image">
+                            <a href="modifyUser.php?id=<?php $row['user_id']; ?>"> <img src="../images/write.png" alt="Modifier">
+                            </a>
+
+                        </td>
+                        <td class="image">
+                            <a href="deleteUser.php?id=<?php $row['user_id']; ?>">
+                                <img src="../images/remove.png" alt="Supprimer">
+                            </a>
+                        </td>
+                    </tr>
+            <?php
+                    }
+                } else {
+                    echo "<p class='message'> Aucun résultat trouvé </p>";
+                }
+            ?>
             </tbody>
         </table>
 
